5a7723e5262d2475f426d6724610acde14ffda91
Pedro <185304469+alvespedro0225@users.noreply.github.com>
2025-04-11 22:25:26 -0300

    init backend e frontend

---
+++ backend/src/pokeAPI/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
